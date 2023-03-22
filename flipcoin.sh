d73e5770692e0ff5ca86e51949790c247b126c6e
sudhakiran5 <you@example.com>
2023-03-22 08:55:55 +0530

    added: multiple times of flipping a coin

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-toss=$(( RANDDOM %2 ))
-if((toss==0))
-then 
-  echo "Heads"
+read -p "Enter no of times to toss:"n heads_count=0
+tails_count=0
+for(( i=1;i<=n;i++))
+do
+  toss=$((RANDOM%2))
+  if((toss==0))
+  then
+    echo "flip-$i heads"
+    ((tails_count++))
+  fi
+done
+echo "The $heads_count is $heads_count and tails count is $tails_count"
+if((heads_count>tails_count))
+  echo "winner is heads"
+elif((tails_count>heads_count))
+then
+  echo "winner is tails"
 else
-  echo "Tails"
+  echo "its tie"
 fi
